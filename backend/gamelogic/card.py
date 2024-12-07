6b77b4382e26714dc108ec568615f0a6c23d55de
Abu Tayyab <144416291+ABUTAYYAB@users.noreply.github.com>
2024-12-07 08:34:44 +0500

    [UPDATED] Card Class

--- backend/gamelogic/card.py
+++ backend/gamelogic/card.py
@@ -6,19 +6,59 @@ class Card:
         self.color = color
         self.CardName = suit + "_" +rank
         self.visible = False
+        self.Joker = False
 
 
-    #Basic fu ctions for thecard Classs
+    #Basic functions for thecard Classs
 
+    def getCardName(self):
+        return self.CardName
+    
     def getSuit(self):
         return self.suit
+    
     def isVisible(self):
         return self.visible
+    
+    def isJoker(self):
+        return self.isJoker
+    
+    def makeJoker(self):
+        self.isJoker = True
 
     def isRed(self):
         return self.color == "red"
+    
     def isBlack(self):
         return self.color == "black"
+    
+    def GetPoints(self):
+        if self.rank =="A":
+            return 10
+        elif self.rank == "2":
+            return 2
+        elif self.rank == "3":
+            return 3
+        elif self.rank == "4":
+            return 4
+        elif self.rank == "5":
+            return 5
+        elif self.rank == "6":
+            return 6
+        elif self.rank == "7":
+            return 7
+        elif self.rank == "8":
+            return 8
+        elif self.rank == "9":
+            return 9
+        elif self.rank == "10":
+            return 10
+        elif self.rank == "J":
+            return 10
+        elif self.rank == "Q":
+            return 10
+        elif self.rank == "K":
+            return 10
     
     def getRank(self):
         if self.rank =="A":
