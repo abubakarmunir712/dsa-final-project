6d966e03f506cad7967cb0036155515e0c4717ea
aabr2612 <aabr2612@gmail.com>
2024-12-08 16:37:15 +0500

    Sequence constructor updated

--- backend/classes/sequence.py
+++ backend/classes/sequence.py
@@ -3,10 +3,10 @@ from card import Card
 
 # Sequence
 class Sequence:
-    def __init__(self, cards=None):
+    def __init__(self, cards=None, first_life=False, second_life=False):
         self.__cards = cards
         self.__sequence = None
-        self.calculate_sequence(False, False)
+        self.calculate_sequence(first_life, second_life)
 
     # testing bubble sort
     def bubble_sort(self, cards):
@@ -36,7 +36,7 @@ class Sequence:
 
         # If first life does not exist
         if not first_life:
-            if self.is_first_life():
+            if self.is_pure_sequence():
                 self.__sequence = "First life"
                 return
 
@@ -45,6 +45,11 @@ class Sequence:
             if self.is_second_life():
                 self.__sequence = "Second life"
                 return
+
+        # If first and second life already exists then check for the pure sequence
+        if self.is_pure_sequence():
+            self.__sequence = "Pure sequence"
+            return
 
     # Checking pure sequence (Checks first life because first life is a pure sequence)
     def is_pure_sequence(self):
@@ -67,6 +72,8 @@ class Sequence:
     # Checking if the suit and rank are valid for pure sequence
     def check_suit_rank(self, card1, card2):
         if not card1.get_suit() == card2.get_suit():
+            return False
+        if card1.get_rank() == card2.get_rank() - 1:
             return False
         if not card1.get_rank() == card2.get_rank() - 1:
             return False
