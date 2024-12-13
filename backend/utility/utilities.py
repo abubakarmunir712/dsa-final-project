c839c19f99236a527d6d610a6a9cb09471cb2157
Abu Tayyab <144416291+ABUTAYYAB@users.noreply.github.com>
2024-12-13 16:04:26 +0500

    [ADDED] min and max rank functionality

--- backend/utility/utilities.py
+++ backend/utility/utilities.py
@@ -1,16 +1,46 @@
-def bubble_sort(cards):
+from typing import List
+from classes.card import Card
+
+
+def bubble_sort(cards: List[Card]):
     for i in range(len(cards)):
         for j in range(len(cards) - i - 1):
-            if cards[j].get_rank() > cards[j+1].get_rank():
-                cards[j], cards[j+1] = cards[j+1], cards[j]
+            if cards[j].get_rank() > cards[j + 1].get_rank():
+                cards[j], cards[j + 1] = cards[j + 1], cards[j]
     return cards
 
-def insertion_sort(cards):
+
+def insertion_sort(cards: List[Card]):
     for i in range(1, len(cards)):
         key = cards[i]
         j = i - 1
         while j >= 0 and key.get_rank() < cards[j].get_rank():
-            cards[j+1] = cards[j]
+            cards[j + 1] = cards[j]
             j -= 1
-        cards[j+1] = key
+        cards[j + 1] = key
     return cards
+
+
+def find_max_rank(cards: List[Card], can_be_joker):
+    cards = bubble_sort(cards)
+    for i in range(len(cards) - 1, -1, -1):
+
+        if (
+            cards[i].get_rank() != 1
+            and cards[i].get_rank() != 14
+            and (not cards[i].is_joker() or can_be_joker)
+        ):
+            return cards[i].get_rank()
+    return -1
+
+
+def find_min_rank(cards: List[Card], can_be_joker):
+    cards = bubble_sort(cards)
+    for i in range(len(cards)):
+        if (
+            cards[i].get_rank() != 14
+            and cards[i].get_rank() != 1
+            and (not cards[i].is_joker() or can_be_joker)
+        ):
+            return cards[i].get_rank()
+    return -1
