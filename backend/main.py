d5c227b14d3b036cf90e1a6317617f05a79a6a59
aabr2612 <aabr2612@gmail.com>
2024-12-14 15:15:45 +0500

    game initialized

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,9 @@ from game_logic.game import Game
 from classes.player import Player
 from classes.card import Card
 
-a = [
-    Card("hearts", "A"),
-    Card("hearts", "Q",True),
-    Card("hearts", "K"),
-    Card("spades", "2"),
-    Card("clubs", "4"),
-    Card("spades", "3"),
-    Card("spades", "4"),
-    Card("hearts", "J"),
-]
-
-p = Player(a)
+def main():
+    game = Game(2,0)
+    
+    
+if __name__ == "__main__":
+    main()
