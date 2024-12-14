83972e3bd893cad117716ea093a0bef4333aa019
Abu Tayyab <144416291+ABUTAYYAB@users.noreply.github.com>
2024-12-14 17:34:38 +0500

    [STARTED] working on bot functionality

---
+++ backend/bot/bot.py
@@ -0,0 +1,12 @@
+from backend.classes.player import Player
+from backend.classes.deck import Deck
+from backend.classes.card import Card
+
+
+class Bot(Player):
+    def __init__(self, cards, name="Unknown", is_AI=True, player_id=None):
+        super().__init__(cards, name, is_AI, player_id)
+
+    def play_yourself():
+        pass
+     
