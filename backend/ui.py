452c82b989d59c15e7bf98a4422ecda504073cc8
Abu Tayyab <144416291+ABUTAYYAB@users.noreply.github.com>
2024-12-15 18:20:26 +0500

    [FIXED] Interfaces Issues

--- backend/ui.py
+++ backend/ui.py
@@ -1,8 +1,11 @@
 from game_logic.game import Game
 import ui_utility
 from time import sleep
+from classes.card import Card
+from classes.player import Player
 
-
+from enums.rank_enum import Rank
+from enums.suit_enum import Suit
 # Main function
 def main():
     # Main loop
@@ -14,105 +17,175 @@ def main():
         # Game play
         if choice == "1":
 
-            ui_utility.header()
-
-            # Input the number of players and AI players
-            no_of_players, no_of_ai_players = ui_utility.game_players()
-            
-            # Creating a new game and initializing the player turn and card drawn for better turn management of player
-            game = Game(no_of_players, no_of_ai_players)
-            player_turn = game.players_list.pop(0)
-            turn_change = False
-            is_card_drawn = False
-            
-            # Game loop
-            while True:
+            try:
                 ui_utility.header()
-                # Printing the joker card
-                print(f"{game.joker} is the joker")
-                
-                # Printing the player's turn and points
-                print(f"""{player_turn.get_name()}'s turn""")
-                print(player_turn.get_points())
-                
-                # Printing the player's hand
-                ui_utility.print_cards(player_turn.get_hand())
-                
-                # Printing the stockpile and waste pile
-                ui_utility.print_waste_and_stock_pile(game.wastepile,game.stockpile)
 
-                if turn_change:
-                    player_turn = game.players_list.pop(0)
-                    turn_change = False
-                
-                player_move = ui_utility.player_choice(is_card_drawn)
-                if player_move == "1" and not is_card_drawn:
-                    player_turn.get_card_from_stockpile(game.stockpile)
-                    print("Getting card from stock pile")
-                    is_card_drawn = True
-                elif player_move == "2" and not is_card_drawn:
-                    if player_turn.get_card_from_wastepile(game.wastepile):
-                        print("Card drawn from waste pile")
-                    else:
-                        player_turn.get_card_from_stockpile(game.stockpile)
-                        print("Card drawn from stock pile")
-                    sleep(1)
-                    is_card_drawn = True
-                elif player_move == "3" and is_card_drawn:
-                    sequence_no = int(
-                        input(
-                            "Enter sequence number from where you want to discard the card: "
-                        )
-                    )
-                    card_name = input("Enter card name to discard: ")
-                    if player_turn.discard_card(
-                        sequence_no - 1, card_name, game.wastepile
-                    ):
-                        print("Card discarded")
+                # Input the number of players and AI players
+                no_of_players, no_of_ai_players = ui_utility.game_players()
+
+                print(no_of_players, no_of_ai_players)
+
+                # Creating a new game and initializing the player turn and card drawn for better turn management of player
+                game = Game(None,no_of_players, no_of_ai_players)
+                turn_change = False
+                is_card_drawn = False
+
+
+                # Initialize the player hand with Card objects
+                player_hand = [
+                    Card(Suit.DIAMONDS.value, Rank.FIVE.value),
+                    Card(Suit.DIAMONDS.value, Rank.SIX.value),
+                    Card(Suit.DIAMONDS.value, Rank.SEVEN.value),
+                    Card(Suit.DIAMONDS.value, Rank.EIGHT.value),
+                    Card(Suit.DIAMONDS.value, Rank.NINE.value),
+                    Card(Suit.DIAMONDS.value, Rank.TEN.value),
+                    Card(Suit.CLUBS.value, Rank.THREE.value),
+                    Card(Suit.DIAMONDS.value, Rank.THREE.value),
+                    Card(Suit.HEARTS.value, Rank.THREE.value),
+                    Card(Suit.SPADES.value, Rank.NINE.value),
+                    Card(Suit.HEARTS.value, Rank.NINE.value),
+                    Card(Suit.CLUBS.value, Rank.NINE.value),
+                    Card(None, None, is_joker=True),  # Joker card
+                ]
+
+                player = Player(player_hand, "Player 1",False)
+                game.players_list.append(player)
+                player_turn = game.get_players_list().pop(0)
+
+                # Game loop
+                while True:
+                    ui_utility.header()
+                    
+                    # Player's turn management
+                    if turn_change:
+                        game.players_list.append(player_turn)
+                        player_turn = game.players_list.pop(0)
+                        turn_change = False
+                        is_card_drawn = False
+                    
+                    # Printing the joker card
+                    print(f"{game.get_wild_joker()} is the joker")
+
+                    # Printing the player's turn and points
+                    print(f"""{player_turn.get_name()}'s turn""")
+                    print(player_turn.get_points())
+
+                    # Printing the player's hand
+                    ui_utility.print_cards(player_turn.get_hand())
+
+                    # Printing the stockpile and waste pile
+                    ui_utility.print_waste_and_stock_pile(game.wastepile, game.stockpile)
+
+
+                    # AI player's turn
+                    if player_turn.is_AI:
+                        player_turn.play()
                         turn_change = True
                         is_card_drawn = False
                         sleep(2)
-                    else:
-                        print("Invalid card")
+                        continue
+
+                    # Human Player's turn
+
+                    # Player's choice
+                    player_move = ui_utility.player_choice(is_card_drawn)
+
+                    # Player's move
+                    
+                    # Draw card from stockpile
+                    if player_move == "1" and not is_card_drawn:
+                        player_turn.get_card_from_stockpile(game.stockpile)
+                        ui_utility.print_statement("Getting card from stock pile")
+                        is_card_drawn = True
+
+                    # Draw card from waste pile
+                    elif player_move == "2" and not is_card_drawn:
+                        if player_turn.get_card_from_wastepile(game.wastepile):
+                            ui_utility.print_statement("Card drawn from waste pile")
+                        else:
+                            player_turn.get_card_from_stockpile(game.stockpile)
+                            ui_utility.print_statement("Card drawn from stock pile")
                         sleep(1)
-                elif player_move == "4":
-                    sequence_no_1 = int(
-                        input(
-                            "Enter sequence number from where you want to move the card: "
+                        is_card_drawn = True
+
+                    # Discard card
+                    elif player_move == "3" and is_card_drawn:
+                        sequence_no,card_name = ui_utility.discard_card_input()
+                        if sequence_no_1 and card_name:
+                            if player_turn.discard_card(
+                                sequence_no - 1, card_name, game.wastepile
+                            ):
+                                ui_utility.print_statement("Card discarded")
+                                turn_change = True
+                                is_card_drawn = False
+                                sleep(2)
+                            else:
+                                ui_utility.print_statement("Invalid card")
+                                sleep(1)
+                        else:
+                            ui_utility.print_statement("Invalid card")
+                            sleep(1)
+
+                    # Move card from one sequence to another
+                    elif player_move == "4":
+                        sequence_no_1, sequence_no_2, card_name = ui_utility.move_card_input()
+                        if sequence_no_1 and sequence_no_2 and card_name:
+                            if player_turn.move_card(
+                                sequence_no_1 - 1, sequence_no_2 - 1, card_name
+                            ):
+                                ui_utility.print_statement("Card moved")
+                            else:
+                                ui_utility.print_statement("Invalid move")
+                            sleep(1)
+
+
+                    # Group cards
+                    elif player_move == "5":
+                        cards_list = []
+                        while True:
+                            sequence_no,card_name = ui_utility.discard_card_input()
+                            if(sequence_no and card_name):
+                                cards_list.append((sequence_no - 1, card_name))
+                                if (
+                                    input(
+                                        "                     Do you want to add more cards to the group? (y/n): "
+                                    ).lower()
+                                    == "n"
+                                ):
+                                    break
+                        if player_turn.group_cards(cards_list):
+                            ui_utility.print_statement("Cards grouped")
+
+                    # Invalid choice
+                    else:
+                        ui_utility.print_statement("Invalid choice")
+                        sleep(1)
+
+                    # Check if the player has won
+                    if game.winner_selection():
+                        ui_utility.print_statement(
+                            f"{player_turn.get_name()} has won the game!"
                         )
-                    )
-                    sequence_no_2 = int(
-                        input("Enter sequence number where you want to move the card: ")
-                    )
-                    card_name = input("Enter card name: ")
-                    player_turn.move_card(
-                        sequence_no_1 - 1, sequence_no_2 - 1, card_name
-                    )
-                elif player_move == "5":
-                    cards_list = []
-                    while True:
-                        sequence_no = int(input("Enter sequence number: "))
-                        card_name = input("Enter card name: ")
-                        cards_list.append((sequence_no - 1, card_name))
-                        if (
-                            input("Do you want to add more cards to the group? (y/n): ")
-                            == "n"
-                        ):
-                            break
-                    player_turn.group_cards(cards_list)
-                if turn_change:
-                    game.players_list.append(player_turn)
-                if game.winner_selection():
-                    print(f"{player_turn.get_name()} has won the game!")
-                    ui_utility.print_cards(player_turn.hand)
-                    break
+                        ui_utility.print_cards(player_turn.get_hand())
+                        sleep(10)
+                        break
+
+            except Exception as e:
+                ui_utility.print_statement(f"An error occurred: {e}")
+                sleep(1)
+
+        # Rules
         elif choice == "2":
             ui_utility.header()
             ui_utility.rules()
+
+        # Exit
         elif choice == "3":
             break
+
+        # Invalid choice
         else:
-            print("                     Invalid choice")
+            ui_utility.print_statement("Invalid choice")
             sleep(1)
 
 
--- backend/ui_utility.py
+++ backend/ui_utility.py
@@ -18,29 +18,29 @@ def header():
 # Player move choice
 def player_choice(is_card_drawn):
     if not is_card_drawn:
-        print("                     1. Draw card from StockPile")
-        print("                     2. Draw card from WastePile")
+        print_statement("1. Draw card from StockPile")
+        print_statement("2. Draw card from WastePile")
     else:
-        print("                     3. Discard card")
+        print_statement("3. Discard card")
 
-    print("                     4. Move card from one sequence to another")
-    print("                     5. Group cards")
-    choice = input("Enter your choice: ")
+    print_statement("4. Move card from one sequence to another")
+    print_statement("5. Group cards")
+    choice = input("                     Enter your choice: ")
     return choice
 
 
 # Main menu
 def main_menu():
-    print("                     1. Play a game")
-    print("                     2. Rules")
-    print("                     3. Exit")
-    choice = input("Enter your choice: ")
+    print_statement("1. Play a game")
+    print_statement("2. Rules")
+    print_statement("3. Exit")
+    choice = input("                     Enter your choice: ")
     return choice
 
 
 # Rules
 def rules():
-    print("                     Rules\n\n")
+    print_statement("Rules\n\n")
     print(
         """
             1. The game is played with two decks of cards with jokers.
@@ -67,42 +67,98 @@ def rules():
     )
     input("Press any key to continue...")
 
-
+# Game players input
 def game_players():
-    print("                     Number of players\n\n")
-    no_of_players = input("Enter number of total players(max. 6): ")
-    if not no_of_players.isdigit():
-        print("Number of players must be an integer")
-    elif int(no_of_players) > 6:
-        print("Number of players must be less than or equal to 6")
-    elif int(no_of_players) < 2:
-        print("Number of players must be greater than or equal to 2")
-    else:
+    print_statement("Number of players\n\n")
+    while True:
+        no_of_players = input("Enter number of total players (max. 6): ").strip()
+        if not no_of_players.isdigit():
+            print_statement("Number of players must be an integer.")
+            continue
         no_of_players = int(no_of_players)
-        no_of_ai_players = input(
-            f"Enter number of AI players (max. {no_of_players-1}): "
-        )
-        if not no_of_ai_players.isdigit():
-            print("Number of AI players must be an integer")
-        elif int(no_of_ai_players) > no_of_players - 1:
-            print(
-                "Number of AI players must be at least 1 less than the total number of players"
-            )
+        if no_of_players > 6:
+            print_statement("Number of players must be less than or equal to 6.")
+        elif no_of_players < 2:
+            print_statement("Number of players must be greater than or equal to 2.")
         else:
-            no_of_ai_players = int(no_of_ai_players)
-            if no_of_ai_players < 1:
-                no_of_ai_players = 0
-            return no_of_players, no_of_ai_players
+            break
 
-    return 0, 0
+    while True:
+        no_of_ai_players = input(
+            f"Enter number of AI players (max. {no_of_players - 1}): "
+        ).strip()
+        if not no_of_ai_players.isdigit():
+            print_statement("Number of AI players must be an integer.")
+            continue
+        no_of_ai_players = int(no_of_ai_players)
+        if no_of_ai_players > no_of_players - 1:
+            print_statement("Number of AI players must be less than the total players.")
+        else:
+            break
+
+    return no_of_players, no_of_ai_players
+
 
 # Printing the waste and stock pile
 def print_waste_and_stock_pile(wastepile, stockpile):
     if wastepile.get_top_card():
-            print(f"Wastepile top card:  {wastepile.get_top_card().card_name}")
+        print(f"Wastepile top card: {wastepile.get_top_card().card_name}")
     else:
         print("Waste pile is empty")
-        
-        
-    if stockpile.get_top_card():
+
+    if not stockpile.is_empty():
         print("Stockpile: Cards available in stock pile")
+
+
+# Print statement
+def print_statement(statement):
+    print("                     " + statement)
+    return
+
+
+# Discard card input
+def discard_card_input():
+    sequence_no = input(
+        "                     Enter sequence number where the card is: "
+    )
+    if not sequence_no.isdigit():
+        print_statement("Invalid sequence number")
+        return None, None
+
+    sequence_no = int(sequence_no)
+    if sequence_no < 1 or sequence_no > 5:
+        print_statement("Invalid sequence number")
+        return None, None
+
+    card_name = input("                     Enter card name: ")
+    return sequence_no, card_name
+
+
+# Move card input
+def move_card_input():
+    sequence_no_1 = input(
+        "                     Enter sequence number from where the card is: "
+    )
+    if not sequence_no_1.isdigit():
+        print_statement("Invalid sequence number")
+        return None, None, None
+
+    sequence_no_1 = int(sequence_no_1)
+    if sequence_no_1 < 1 or sequence_no_1 > 5:
+        print_statement("Invalid sequence number")
+        return None, None, None
+
+    sequence_no_2 = input(
+        "                     Enter sequence number where the card is to be moved: "
+    )
+    if not sequence_no_2.isdigit():
+        print_statement("Invalid sequence number")
+        return None, None, None
+
+    sequence_no_2 = int(sequence_no_2)
+    if sequence_no_2 < 1 or sequence_no_2 > 5:
+        print_statement("Invalid sequence number")
+        return None, None, None
+
+    card_name = input("                     Enter card name: ")
+    return sequence_no_1, sequence_no_2, card_name
